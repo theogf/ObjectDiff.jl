0b9b7fc5f15449951f299a92766ba62c2f28c56d
theogf <theo.galyfajou@gmail.com>
2024-10-16 01:23:25 +0200

    More implementation

--- src/StructDiff.jl
+++ src/StructDiff.jl
@@ -1,43 +1,118 @@
 module StructDiff
 
+using AbstractTrees
+
 export compare
 
-struct TypeDifference
+abstract type AbstractDiff end
+
+Base.isempty(::AbstractDiff) = false
+
+struct TypeDiff <: AbstractDiff
     T1::DataType
     T2::DataType
 end
 
-struct FieldDifferences
-    f1::Vector{Symbol}
-    f2::Vector{Symbol}
+AbstractTrees.nodevalue((; T1, T2)::TypeDiff) = "type: $(T1) ≠ $(T2)"
+
+struct SizeDiff <: AbstractDiff
+    s1::Tuple
+    s2::Tuple
 end
 
-struct BitsDiff
+AbstractTrees.nodevalue((; s1, s2)::SizeDiff) = "size: $(s1) ≠ $(s2)"
+
+struct FieldDiff <: AbstractDiff
+    name::Symbol
+    diff::AbstractDiff
+end
+
+Base.isempty((; diff)::FieldDiff) = isempty(diff)
+name((; name)::FieldDiff) = name
+AbstractTrees.nodevalue((; name, diff)::FieldDiff) = "$(name):"
+AbstractTrees.children((; diff)::FieldDiff) = [diff]
+
+struct FieldsDiff <: AbstractDiff
+    diffs::Vector{FieldDiff}
+end
+
+Base.isempty((; diffs)::FieldsDiff) = isempty(diffs) || all(isempty, diffs)
+AbstractTrees.children((; diffs)::FieldsDiff) = filter(!isempty, diffs)
+function AbstractTrees.nodevalue((; diffs)::FieldsDiff)
+    return "Fields $(map(name, filter(!isempty, diffs)))"
+end
+
+struct ArrayDiff <: AbstractDiff
+    diffs::Vector{AbstractDiff}
+end
+
+Base.isempty((; diffs)::ArrayDiff) = isempty(diffs) || all(isempty, diffs)
+AbstractTrees.children((; diffs)::ArrayDiff) = filter(!isempty, diffs)
+AbstractTrees.nodevalue(::ArrayDiff) = "Array"
+
+struct StructSummary <: AbstractDiff
+    summary::Union{Nothing,String}
+    x::Any
+    y::Any
+    diffs::Vector{AbstractDiff}
+end
+
+Base.isempty((; diffs)::StructSummary) = isempty(diffs) || all(isempty, diffs)
+AbstractTrees.children((; diffs)::StructSummary) = filter(!isempty, diffs)
+function AbstractTrees.nodevalue((; summary, x, y)::StructSummary)
+    return something(summary, "$(x) ≠ $(y)")
+end
+
+struct BitsDiff <: AbstractDiff
     x::Any
     y::Any
 end
 
+AbstractTrees.nodevalue((; x, y)::BitsDiff) = "$x ≠ $y"
+
 # Write your package code here.
 function compare(x::T1, y::T2) where {T1,T2}
-    diffs = []
-    if T1 != T2
-        push!(diffs, TypeDifference(T1, T2))
-    end
+    diffs = AbstractDiff[]
     if isbits(x) && isbits(y)
         if x !== y
-            push!(diffs, BitsDiff(x, y))
+            return BitsDiff(x, y)
         end
     elseif isstructtype(T1) && isstructtype(T2)
+        if T1 != T2
+            push!(diffs, TypeDiff(T1, T2))
+        end
         if fieldnames(T1) == fieldnames(T2)
             fields = map(collect(fieldnames(T1))) do field
-                compare(getfield(x, field), getfield(y, field))
+                FieldDiff(field, compare(getfield(x, field), getfield(y, field)))
             end
-            push!(diffs, fields)
+            diff = FieldsDiff(fields)
+            if !isempty(diff)
+                push!(diffs, diff)
+            end
         else
-            push!(diffs, FieldDifferences(fieldnames(T1), fieldnames(T2)))
+            push!(diffs, compare(collect(fieldnames(T1)), collect(fieldnames(T2))))
         end
+    elseif T1 <: AbstractArray && T2 <: AbstractArray
     end
-    return diffs
+    return StructSummary(nothing, x, y, diffs)
+end
+
+function compare(x::AbstractArray{T1}, y::AbstractArray{T2}) where {T1,T2}
+    diffs = AbstractDiff[]
+    if T1 != T2
+        push!(diffs, TypeDiff(T1, T2))
+    end
+    if size(x) != size(y)
+        push!(diffs, SizeDiff(size(x), size(y)))
+    else
+        fields = ArrayDiff(map(compare, x, y))
+        isempty(fields) || push!(diffs, fields)
+    end
+    return StructSummary(nothing, x, y, diffs)
+end
+
+function Base.show(io::IO, ::MIME"text/plain", diff::AbstractDiff)
+    return print_tree(io, diff)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,25 @@ using Test
 using Aqua
 using JET
 
+struct Foo
+    a
+    b
+end
+struct Bar
+    c
+end
+
 @testset "StructDiff.jl" begin
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(StructDiff)
     end
     @testset "Code linting (JET.jl)" begin
-        JET.test_package(StructDiff; target_defined_modules = true)
+        JET.test_package(StructDiff; target_defined_modules=true)
+    end
+    @testset "Compare testing" begin
+        x = Foo(Foo(2, 4), 5)
+        y = Foo("C", nothing)
+        z = Foo(Foo(2, 4), nothing)
     end
     # Write your tests here.
 end
