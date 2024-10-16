c8241cc2fd468b8f6e15f4afd2513ec443799c2e
Documenter.jl <documenter@juliadocs.github.io>
2024-10-16 07:30:28 +0000

    build based on bfe3d66

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StructDiff","category":"page"},{"location":"#StructDiff","page":"Home","title":"StructDiff","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StructDiff.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StructDiff]","category":"page"}]
+}
