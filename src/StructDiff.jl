88ffd8dfbae880113d60994e4c6b7f23033a6c3f
theogf <theo.galyfajou@gmail.com>
2024-10-19 11:38:03 +0200

    Fixes

--- src/StructDiff.jl
+++ src/StructDiff.jl
@@ -21,7 +21,7 @@ function compare(x::T1, y::T2) where {T1,T2}
         end
         if fieldnames(T1) == fieldnames(T2)
             fields = map(collect(fieldnames(T1))) do field
-                FieldDiff(field, compare(getfield(x, field), getfield(y, field)))
+                NamedDiff(repr(field), compare(getfield(x, field), getfield(y, field)))
             end
             diff = FieldsDiff(fields)
             if !isempty(diff)
@@ -35,18 +35,37 @@ function compare(x::T1, y::T2) where {T1,T2}
     return StructSummary(x, y, diffs, "")
 end
 
-function compare(x::AbstractArray{T1}, y::AbstractArray{T2}) where {T1,T2}
+function compare(x::AbstractArray, y::AbstractArray)
     diffs = AbstractDiff[]
+    T1 = typeof(x)
+    T2 = typeof(y)
     if T1 != T2
         push!(diffs, TypeDiff(T1, T2))
     end
     if size(x) != size(y)
         push!(diffs, SizeDiff(size(x), size(y)))
     else
-        fields = ArrayDiff(map(ComponentDiff, 1:length(x), vec(map(compare, x, y))))
+        indices = repr.(1:length(x))
+        fields = ArrayDiff(map(NamedDiff, indices, vec(map(compare, x, y))))
         isempty(fields) || push!(diffs, fields)
     end
     return StructSummary(x, y, diffs, "")
+end
+
+function compare(d1::AbstractDict, d2::AbstractDict)
+    diffs = AbstractDiff[]
+    T1 = typeof(d1)
+    T2 = typeof(d2)
+    if T1 != T2
+        push!(diffs, TypeDiff(T1, T2))
+    end
+    if length(d1) != length(d2)
+        push!(diffs, SizeDiff((length(d1),), (length(d2),)))
+    end
+    if d1 != d2
+        push!(diffs, DictDiff(d1, d2))
+    end
+    return StructSummary(d1, d2, diffs, "")
 end
 
 function Base.show(io::IO, ::MIME"text/plain", diff::AbstractDiff; maxdepth=10)
